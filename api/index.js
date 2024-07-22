39e18b3f63d847363fe434ba5192d99b7520aaa1
bensebgagmohammed <bensbgag258@gmail.com>
2024-07-22 14:12:48 +0100

    mvc folders and setup booking route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+import bookingRouter from "./routes/bookingRoutes";
 const app = express();
 dotenv.config();
 
@@ -13,6 +13,9 @@ const connect = async () => {
     console.log(e);
   }
 };
+
+app.use(express.json({ limit: "10kb" }));
+app.use("/api/v1/bookings", bookingRouter);
 
 app.listen(8800, () => {
   console.log("connect ");
